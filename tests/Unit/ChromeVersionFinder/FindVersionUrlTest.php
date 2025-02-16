c5e72bfcf130a03dce53bbef2b920d5c36f7adc3
Mior Muhammad Zaki <crynobone@gmail.com>
2025-02-16 20:02:58 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Unit/ChromeVersionFinder/FindVersionUrlTest.php
+++ tests/Unit/ChromeVersionFinder/FindVersionUrlTest.php
@@ -42,11 +42,31 @@ it('can get version when given version is not a valid `ctype_digit()`', function
     ['133a', '133a'],
 ]);
 
-it('can get legacy versions', function (int $milestone, string $version) {
+it('can get legacy version (43-69)', function (int $given, string $version) {
     $finder = new ChromeVersionFinder;
 
-    expect($finder->findVersionUrl($milestone))->toBe($version);
+    expect($finder->findVersionUrl($given))->toBe($version);
 })->with([
+    [43, '2.20'],
+    [50, '2.22'],
     [60, '2.33'],
+    [69, '2.44'],
+]);
+
+it('can get previous version (70-114)', function (int $given, string $version) {
+    HttpClient::$instance->shouldReceive('get')
+        ->once()
+        ->with(\sprintf('https://chromedriver.storage.googleapis.com/LATEST_RELEASE_%d', $given), ['verify' => true])
+        ->andReturn(new Response(body: (string) $given));
+
+    $finder = new ChromeVersionFinder;
+
+    expect($finder->findVersionUrl($given))->toBe($version);
+})->with([
+    [70, '70'],
+    [80, '80'],
+    [90, '90'],
+    [100, '100'],
+    [114, '114'],
 ]);
 
