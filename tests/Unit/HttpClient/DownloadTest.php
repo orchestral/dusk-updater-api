45c6e1770daab6514b72f6479b2508ad4088c9d7
Mior Muhammad Zaki <crynobone@gmail.com>
2025-02-17 07:54:54 +0800

    Test Improvements
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Unit/HttpClient/DownloadTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use Mockery as m;
+use Orchestra\DuskUpdaterApi\HttpClient;
+
+use function Orchestra\DuskUpdaterApi\join_paths;
+
+beforeEach(function () {
+    HttpClient::$instance = m::mock(Client::class);
+});
+
+afterEach(function () {
+    HttpClient::flushState();
+});
+
+it('can download a file', function () {
+    HttpClient::$instance->shouldReceive('get')
+        ->with('https://storage.googleapis.com/chrome-for-testing-public/113.0.5672.63/linux64/chrome-linux64.zip', m::type('Array'))
+        ->once()
+        ->andReturn(new Response(body: 'Downloaded'));
+
+    HttpClient::download(
+        'https://storage.googleapis.com/chrome-for-testing-public/113.0.5672.63/linux64/chrome-linux64.zip',
+        $file = join_paths(__DIR__, 'tmp', 'chrome-linux.zip')
+    );
+
+    $this->assertFileExists($file);
+
+    unlink($file);
+});
